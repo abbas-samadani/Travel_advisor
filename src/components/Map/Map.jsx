aa2501315f4c0887b0bbbff767e71be89b483d6b
Abbas Samadani <73225830+abbas-samadani@users.noreply.github.com>
2022-04-17 00:19:15 +0100

    initializian map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './Styles';
 
 function Map() {
+
+    const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+
+  const cordinate = {lat:0 , lng:0}
+
     return (
-        <h1>
-            Map
-        </h1>
+        <div className={classes.mapContainer}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyCGhuRffFZMBtq784yAUdD8ONobfM9xN5A" }}
+          defaultCenter={cordinate}
+          center={cordinate}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          options={''}
+          onChange={''}
+          onChildClick={''}
+        >
+        </GoogleMapReact>
+      </div>
     )
 }
 
